6c0040e851645970e69963d2de7264a43c812478
Joel Leibow <joel.leibow@workiva.com>
2017-11-28 21:57:35 -0700

    Use JS for CustomEvent

---
+++ lib/src/create_custom_event.dart
@@ -0,0 +1,29 @@
+@JS()
+library shell_events.init;
+
+import 'dart:html';
+
+import 'package:js/js.dart';
+
+@JS('eval')
+external dynamic _eval(String source);
+
+final Function _jsCreateCustomEvent = _eval('''
+  (function _jsCreateCustomEvent(type,
+      detail,
+      bubbles,
+      cancelable,
+      composed
+  ) {
+    return new CustomEvent(type, {
+      detail: detail,
+      bubbles: bubbles,
+      cancelable: cancelable,
+      composed: composed,
+    });
+  })
+''');
+
+CustomEvent createCustomEvent(String type, { Map detail, bool bubbles: true, bool cancelable: true, bool composed: true }) {
+  return _jsCreateCustomEvent(type, detail, bubbles, cancelable, composed);
+}
