0f8dfdf239256a26caa530961d29c657de1ec3c4
Joel Leibow <joel.leibow@workiva.com>
2017-12-03 21:28:26 -0700

    Remove unneeded payload from toggle message

--- lib/src/shell_toggle_messages_event.dart
+++ lib/src/shell_toggle_messages_event.dart
@@ -4,7 +4,7 @@ import './create_custom_event.dart';
 import './shell_event_constants.dart';
 
 class ShellToggleMessagesEvent extends CustomEvent {
-  factory ShellToggleMessagesEvent({Map detail: null}) {
-    return createCustomEvent(ShellEventConstants.TOGGLE_MESSAGES.event, detail: detail);
+  factory ShellToggleMessagesEvent() {
+    return createCustomEvent(ShellEventConstants.TOGGLE_MESSAGES.event);
   }
 }
