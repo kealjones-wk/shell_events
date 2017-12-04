4c9444f06dcbb8a97c3f4e706595edec690e7b4f
Joel Leibow <joel.leibow@workiva.com>
2017-12-03 21:29:05 -0700

    Post message event accepts detail of type String

--- lib/src/shell_post_message_event.dart
+++ lib/src/shell_post_message_event.dart
@@ -4,7 +4,7 @@ import './create_custom_event.dart';
 import './shell_event_constants.dart';
 
 class ShellPostMessageEvent extends CustomEvent {
-  factory ShellPostMessageEvent({Map detail: null}) {
+  factory ShellPostMessageEvent({String detail}) {
     return createCustomEvent(ShellEventConstants.POST_MESSAGE.event, detail: detail);
   }
 }
